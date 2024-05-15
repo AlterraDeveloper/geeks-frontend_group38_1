f8846438e2c217ed9afb26df4902ed258211020e
Alterra$Developer <42990792+AlterraDeveloper@users.noreply.github.com>
2024-05-15 18:56:02 +0600

    lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,74 @@
+
+
+// V8 - JavaScript Engine - движок
+
+// Node.js
+
+// alert("Hello JavaScript!");
+//
+// console.log("Hello JavaScript!");
+
+// Хранение информации
+// Обработка информации
+// Передача информации
+// Защита информации
+
+
+// var, let, const
+
+let userName = "Evgeniy";
+let userSurname = "Kiselev";
+let userFatherName = "Dmitrievich";
+
+let activeGroupCount = 2;
+let studentsCount = 14 + 15;
+let activeMonth = 2;
+
+let groupName = "GEEKS-01F010124";
+
+let courseTitle = "JavaScript. Basics";
+
+let hasEducation = true; // 1
+let hasDoctorDegree = false; // 0
+
+let termsAgree = true;
+
+let birthDate;
+let salary = null;
+
+console.log("My birth date is:", birthDate);
+console.log("My salary:", salary);
+
+let passport = {
+  series: "AN",
+  number: "123456",
+  issueDate: "01.01.15",
+  expireDate: "01.01.25",
+  issueAuth: "Guvd bishkek"  
+};
+
+let aboutMe = {
+    name: "",
+    surname: "",
+
+}
+
+
+// Data types - типы данных
+
+// 1. string - текстовый, строковый
+// 2. number - числовой
+// 3. boolean - логический
+// 4. undefined - неопределенный
+// 5. null - пустой
+// 6. object - объект
+// ----------
+// 7. BigInt
+// 8. Symbol
+
+
+
+
+
+
+
